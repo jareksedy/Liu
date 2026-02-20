e0a1738d20ac0341d635786088dd8720ed35e586
Yaroslav Sedyshev <yar.sedyshev@gmail.com>
2026-02-20 22:50:35 +0500

    Initial Commit

---
+++ liu/liuApp.swift
@@ -0,0 +1,17 @@
+//
+//  liuApp.swift
+//  liu
+//
+//  Created by Ярослав on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct liuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
